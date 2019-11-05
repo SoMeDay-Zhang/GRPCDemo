1a25a45b43a82bd9fc5b25a8dc9467be0b9d4c05
Zhangx <zhangqiaoxiangyang@hotmail.com>
2019-11-05 20:16:19 +0800

    1

--- Address.Api/Program.cs
+++ Address.Api/Program.cs
@@ -23,8 +23,6 @@ namespace Address.Api
                         options.Listen(IPAddress.Any, 5001, listenOptions =>
                         {
                             listenOptions.Protocols = HttpProtocols.Http2;
-                            listenOptions.UseHttps("Certs/addressservicecert.pfx",
-                                "123456");
                         });
                     });
                     webBuilder.UseStartup<Startup>();
